ab4e145bf0c1327c80b13c16facebe2063c3d00f
Nikolaj-Hvitfeldt <nhvitfeldt1@gmail.com>
2025-12-30 17:57:35 +0100

    feat: added final wouldILieImages

--- QuarterMark.Web/src/data/wouldILieImages.ts
+++ QuarterMark.Web/src/data/wouldILieImages.ts
@@ -8,52 +8,52 @@ export interface WouldILieImage {
 export const WOULD_I_LIE_IMAGES: WouldILieImage[] = [
   {
     id: '1',
-    imageUrl: '/images/would-i-lie-images/person9.jpg',
+    imageUrl: '/images/would-i-lie-images/person5.jpg',
     description: 'Person 1',
   },
   {
     id: '2',
-    imageUrl: '/images/would-i-lie-images/person3.jpg',
+    imageUrl: '/images/would-i-lie-images/person6.jpg',
     description: 'Person 2',
   },
   {
     id: '3',
-    imageUrl: '/images/would-i-lie-images/person10.jpg',
+    imageUrl: '/images/would-i-lie-images/person9.jpg',
     description: 'Person 3',
   },
   {
     id: '4',
-    imageUrl: '/images/would-i-lie-images/person1.jpg',
+    imageUrl: '/images/would-i-lie-images/person3.jpg',
     description: 'Person 4',
   },
   {
     id: '5',
-    imageUrl: '/images/would-i-lie-images/person7.jpg',
+    imageUrl: '/images/would-i-lie-images/person10.jpg',
     description: 'Person 5',
   },
   {
     id: '6',
-    imageUrl: '/images/would-i-lie-images/person4.jpg',
+    imageUrl: '/images/would-i-lie-images/person1.jpg',
     description: 'Person 6',
   },
   {
     id: '7',
-    imageUrl: '/images/would-i-lie-images/person8.jpg',
+    imageUrl: '/images/would-i-lie-images/person7.jpg',
     description: 'Person 7',
   },
   {
     id: '8',
-    imageUrl: '/images/would-i-lie-images/person2.jpg',
+    imageUrl: '/images/would-i-lie-images/person4.jpg',
     description: 'Person 8',
   },
   {
     id: '9',
-    imageUrl: '/images/would-i-lie-images/person7.jpg',
+    imageUrl: '/images/would-i-lie-images/person8.jpg',
     description: 'Person 9',
   },
   {
     id: '10',
-    imageUrl: '/images/would-i-lie-images/person8.jpg',
+    imageUrl: '/images/would-i-lie-images/person2.jpg',
     description: 'Person 10',
   },
 ];
